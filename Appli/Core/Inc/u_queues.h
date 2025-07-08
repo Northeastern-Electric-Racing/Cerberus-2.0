8f4bc87b25e0cf74ff9222fefb132c85e7f273b2
bjackson312006 <blakejackson312006@gmail.com>
2025-07-08 16:23:36 -0400

    Got rid of weird ethernet queue system and replaced it with new u_queues.c ThreadX system

--- Appli/Core/Inc/u_queues.h
+++ Appli/Core/Inc/u_queues.h
@@ -3,20 +3,26 @@
 
 #include "tx_api.h"
 #include "u_config.h"
+#include "u_ethernet.h"
 #include <stdint.h>
-#include <stdio.h>
+
+/*
+*   Basically just a wrapper for ThreadX stuff. Lets you create/configure queues.
+*   
+*   Author: Blake Jackson
+*/
 
 typedef struct {
     TX_QUEUE *queue;           /* Pointer to the queue */
     CHAR     *name;            /* Name of the queue */
-    UINT      priority;        /* Priority of the queue */
     UINT      message_size;    /* Size of each message in the queue, in bytes. */
     UINT      capacity;        /* Maximum number of messages in the queue */
     UCHAR    *memory;          /* Pointer to the queue's memory */
 } queue_t;
 
 /* Queue List */
-extern TX_QUEUE queue_template;
+extern TX_QUEUE eth_incoming; // Incoming Ethernet Queue
+extern TX_QUEUE eth_outgoing; // Outgoing Ethernet Queue
 // add more as necessary
 
 /* API */
@@ -24,4 +30,4 @@ uint8_t queues_init(); // Initializes all queues. Called from app_threadx.c
 uint8_t queue_send(TX_QUEUE *queue, void *message); // Sends a message to the specified queue.
 uint8_t queue_receive(TX_QUEUE *queue, void *message); // Receives a message from the specified queue.
 
-#endif /* u_threads.h */
+#endif /* u_queues.h */
