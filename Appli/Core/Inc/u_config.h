b1ce77819c6b29658e29436ee2028afa4edfd53c
bjackson312006 <blakejackson312006@gmail.com>
2025-07-09 14:04:57 -0400

    First code gen

--- Appli/Core/Inc/u_config.h
+++
@@ -1,3 +0,0 @@
-/* General-purpose macros that can be used throughout the project. */
-#define U_SUCCESS   0
-#define U_ERROR     1
--- Appli/Core/Inc/u_messages.h
+++
@@ -1,6 +0,0 @@
-#ifndef __MESSAGES_H
-#define __MESSAGES_H
-
-void messages_handler(ethernet_message_t *message);
-
-#endif /* u_messages.h */
