f0e5ad994a86d39f2d72a07a40d5d502b7d86c16
bjackson312006 <blakejackson312006@gmail.com>
2025-07-10 13:26:40 -0400

    Added DEBUG_PRINT() to u_config.h

--- Core/Inc/u_config.h
+++ Core/Inc/u_config.h
@@ -1,3 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+
 /* General-purpose macros that can be used throughout the project. */
 #define U_SUCCESS   0
 #define U_ERROR     1
+
+/* Debugging Macros */
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) /* Gets the name of the file. */
+#define DEBUG /* Comment out to enable/disable debugging. */
+#ifdef DEBUG
+    #define DEBUG_PRINT(message, ...) printf("[%s/%s()] " message "\n", __FILENAME__, __func__, ##__VA_ARGS__) /* Prints an error message in the format: "[file_name.c/function()] {message}"*/
+#else
+    #define DEBUG_PRINT(message, ...) /* If debugging is turned off, macro doesn't need to expand to anything. */
+#endif
