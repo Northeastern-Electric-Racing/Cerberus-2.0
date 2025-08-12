1f2a5f3119d150da3f1c551dcf8bda6da2f40742
bjackson312006 <blakejackson312006@gmail.com>
2025-08-12 18:58:40 -0400

    chud software

--- Core/Inc/u_queues.h
+++ Core/Inc/u_queues.h
@@ -16,20 +16,27 @@
 #define QUEUE_WAIT_TIME     MS_TO_TICKS(100) // Wait 100ms for queue stuff before timing out
 
 typedef struct {
-    TX_QUEUE *queue;           /* Pointer to the queue */
-    CHAR     *name;            /* Name of the queue */
-    UINT      message_size;    /* Size of each message in the queue, in bytes. */
-    UINT      capacity;        /* Maximum number of messages in the queue */
+    TX_QUEUE _TX_QUEUE;      /* Queue instance. */
+    size_t   bytes;           /* Size of each queue message, in bytes. */
+    size_t   words;           /* Size of each queue message, in 32-bit words. */
 } queue_t;
 
 /* Queue List */
-extern TX_QUEUE eth_incoming; // Incoming Ethernet Queue
-extern TX_QUEUE eth_outgoing; // Outgoing Ethernet Queue
+extern queue_t eth_incoming; // Incoming Ethernet Queue
+extern queue_t eth_outgoing; // Outgoing Ethernet Queue
 // add more as necessary
 
 /* API */
 uint8_t queues_init(); // Initializes all queues. Called from app_threadx.c
-uint8_t queue_send(TX_QUEUE *queue, void *message); // Sends a message to the specified queue.
-uint8_t queue_receive(TX_QUEUE *queue, void *message); // Receives a message from the specified queue.
+uint8_t queue_send(queue_t *queue, void *message); // Sends a message to the specified queue.
+uint8_t queue_receive(queue_t *queue, void *message); // Receives a message from the specified queue.
+
+/* Struct for configuring queues. */
+typedef struct {
+    queue_t  *queue;         /* Pointer to the queue instance */
+    CHAR     *name;          /* Name of the queue */
+    UINT      message_size;  /* Size of each message in the queue, in bytes. */
+    UINT      capacity;      /* Maximum number of messages in the queue */
+} QUEUE_CONFIG;
 
 #endif /* u_queues.h */
