7f9a96febebc11e7b4a561c62f0d4fec35a3dea1
bjackson312006 <blakejackson312006@gmail.com>
2025-08-14 13:29:51 -0400

    Added DEBUG_PRINT() stuff back in app_azure_rtos.c

--- AZURE_RTOS/App/app_azure_rtos.c
+++ AZURE_RTOS/App/app_azure_rtos.c
@@ -117,7 +117,7 @@ VOID tx_application_define(VOID *first_unused_memory)
   if (tx_byte_pool_create(&nx_app_byte_pool, "Nx App memory pool", nx_byte_pool_buffer, NX_APP_MEM_POOL_SIZE) != TX_SUCCESS)
   {
     /* USER CODE BEGIN NX_Byte_Pool_Error */
-
+    DEBUG_PRINT("ERROR: Failed to create NetXDuo byte pool.");
     /* USER CODE END NX_Byte_Pool_Error */
   }
   else
@@ -131,8 +131,10 @@ VOID tx_application_define(VOID *first_unused_memory)
     if (status != NX_SUCCESS)
     {
       /* USER CODE BEGIN  MX_NetXDuo_Init_Error */
+      DEBUG_PRINT("ERROR: Failed to initialize NetXDuo application (Status: %d).", status);
       while(1)
       {
+        // fatal error so loop forever. unless we just don't want to use NetX/ethernet stuff.
       }
       /* USER CODE END  MX_NetXDuo_Init_Error */
     }
