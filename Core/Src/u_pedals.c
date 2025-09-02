d79aa947d6ffb445c0f0e0ee59403d0cf30194a9
bjackson312006 <blakejackson312006@gmail.com>
2025-09-02 18:42:41 -0400

    its not gonna build because of the timer.h include thing

--- Core/Src/u_pedals.c
+++ Core/Src/u_pedals.c
@@ -1,4 +1,6 @@
 #include "u_pedals.h"
+#include "u_queues.h"
+#include "u_faults.h"
 
 /* =================================== */
 /*            CONFIG MACROS            */
@@ -40,3 +42,48 @@
 #define BRAKE_FAULT_DEBOUNCE        300  // (ms). Debounce time for brake faults.
 #define APPS_THRESHOLD_TOLERANCE    0.45 // (Volts). Tolerance margin around the accelerator pedal.
 #define BRAKE_THRESHOLD_TOLERANCE   0.25 // (Volts). Tolerance margin around the brake pedal.
+
+/* Fault Debounce Callbacks */
+static void _open_circuit_fault_callback(void *arg) {queue_send(&faults, &(fault_t){ONBOARD_PEDAL_OPEN_CIRCUIT_FAULT});}; // Queues the Open Circuit Fault.
+static void _short_circuit_fault_callback(void *arg) {queue_send(&faults, &(fault_t){ONBOARD_PEDAL_SHORT_CIRCUIT_FAULT});}; // Queues the Short Circuit Fault.
+static void _pedal_difference_fault_callback(void *arg) (queue_send(&faults, &(fault_t){ONBOARD_PEDAL_DIFFERENCE_FAULT});); // Queues the Pedal Difference Fault.
+
+/* Calculates brake faults. */
+void calculate_brake_faults(float voltage_brake1, float voltage_brake2) {
+    /* Debounce Timers */
+    static nertimer_t open_circuit_timer;  // Timer for the Open Circuit Fault
+    static nertimer_t short_circuit_timer; // Timer for the Short Circuit Fault
+
+    /* EV3.5.4: For analog acceleration control signals, this error checking must detect open circuit, short to ground and short to sensor power. */
+    
+    /* Open Circuit Fault */
+    bool open_circuit_fault = (voltage_brake1 > BRAKE_SENSOR_IRREGULAR_HIGH + BRAKE_THRESHOLD_TOLERANCE) || (voltage_brake2 > BRAKE_SENSOR_IRREGULAR_HIGH + BRAKE_THRESHOLD_TOLERANCE);
+    debounce(open_circuit_fault, &_open_circuit_fault_callback, BRAKE_FAULT_DEBOUNCE, NULL);
+
+    /* Short Circuit Fault */
+    bool short_circuit_fault = (voltage_brake1 < BRAKE_SENSOR_IRREGULAR_LOW - BRAKE_THRESHOLD_TOLERANCE) || (voltage_brake2 < BRAKE_SENSOR_IRREGULAR_LOW - BRAKE_THRESHOLD_TOLERANCE);
+    debounce(short_circuit_fault, &_short_circuit_fault_callback, BRAKE_FAULT_DEBOUNCE, NULL);
+}
+
+/* Calculates Pedal Faults. */
+void calculate_pedal_faults(float voltage_accel1, float voltage_accel2, float percentage_accel1, float percentage_accel2) {
+    /* Debounce Timers */
+    static nertimer_t open_circuit_timer;     // Timer for the Open Circuit Fault
+    static nertimer_t short_circuit_timer;    // Timer for the Short Circuit Fault
+    static nertimer_t pedal_difference_timer; // Timer for the Pedal Difference Fault
+
+    /* EV3.5.4: For analog acceleration control signals, this error checking must detect open circuit, short to ground and short to sensor power. */
+
+    /* Open Circuit Fault */
+    bool open_circuit_fault = (voltage_accel1 > MAX_VOLTS_UNSCALED - APPS_THRESHOLD_TOLERANCE) || (voltage_accel2 > MAX_VOLTS_UNSCALED - APPS_THRESHOLD_TOLERANCE);
+    debounce(open_circuit_fault, &_open_circuit_fault_callback, PEDAL_FAULT_DEBOUNCE, NULL);
+
+    /* Short Circuit Fault */
+    bool short_circuit_fault = (voltage_accel1 < MIN_APPS1_VOLTS - APPS_THRESHOLD_TOLERANCE) || (voltage_accel2 < MIN_APPS2_VOLTS - APPS_THRESHOLD_TOLERANCE);
+    debounce(short_circuit_fault, &_short_circuit_fault_callback, PEDAL_FAULT_DEBOUNCE, NULL);
+
+    /* Pedal Difference Fault */
+    /* Detects if the two accelerator pedal sensors give readings that differ by more than PEDAL_DIFF_THRESH. */
+    bool pedal_difference_fault = fabs(percentage_accel1 - percentage_accel2) > PEDAL_DIFF_THRESH;
+    debounce(pedal_difference_fault, &_pedal_difference_fault_callback, PEDAL_FAULT_DEBOUNCE, NULL);
+/}
