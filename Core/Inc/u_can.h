79e9f7bf93448324241a6828a11f870d02093ad8
Blake Jackson <blakejackson312006@gmail.com>
2025-09-19 10:50:26 -0500

    Added eFuse CAN messages (#49)

--- Core/Inc/u_can.h
+++ Core/Inc/u_can.h
@@ -10,13 +10,7 @@ uint8_t can1_init(FDCAN_HandleTypeDef *hcan);
 extern can_t can1;
 // add more as necessary
 
-/* List of CAN IDs */
-#define CANID_FAULT_MSG	       0x502
-#define CANID_SHUTDOWN_MSG	   0x123
-#define CANID_PEDALS_VOLTS_MSG 0x504
-#define CAN_ID_PEDALS_NORM_MSG 0x505
-#define CANID_BMS_CELL_TEMPS   0x84
-#define CANID_BMS_DCL_MSG      0x156
+/* DTI CAN IDs */
 #define DTI_CANID_ERPM	      0x416 /* ERPM, Duty, Input Voltage */
 #define DTI_CANID_CURRENTS    0x436 /* AC Current, DC Current */
 #define DTI_CANID_ERPM	      0x416 /* ERPM, Duty, Input Voltage */
@@ -24,6 +18,25 @@ extern can_t can1;
 #define DTI_CANID_TEMPS_FAULT 0x456 /* Controller Temp, Motor Temp, Faults */
 #define DTI_CANID_ID_IQ	      0x476 /* Id, Iq values */
 #define DTI_CANID_SIGNALS 	  0x496 /* Throttle signal, Brake signal, IO, Drive enable */
-// add more as necessary
+
+/* eFuse CAN IDs */
+#define CANID_EFUSE_DASHBOARD 0xEF0
+#define CANID_EFUSE_BRAKE     0xEF1
+#define CANID_EFUSE_SHUTDOWN  0xEF2
+#define CANID_EFUSE_LV        0xEF3
+#define CANID_EFUSE_RADFAN    0xEF4
+#define CANID_EFUSE_FANBATT   0xEF5
+#define CANID_EFUSE_PUMP1     0xEF6
+#define CANID_EFUSE_PUMP2     0xEF7
+#define CANID_EFUSE_BATTBOX   0xEF8
+#define CANID_EFUSE_MC        0xEF9
+
+/* Misc CAN IDs */
+#define CANID_FAULT_MSG	       0x502
+#define CANID_SHUTDOWN_MSG	   0x123
+#define CANID_PEDALS_VOLTS_MSG 0x504
+#define CAN_ID_PEDALS_NORM_MSG 0x505
+#define CANID_BMS_CELL_TEMPS   0x84
+#define CANID_BMS_DCL_MSG      0x156
 
 #endif /* u_can.h */
