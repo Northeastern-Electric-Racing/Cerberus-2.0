c3cc291428188b6fd4bfafe4aed68b9e80e49924
bjackson312006 <blakejackson312006@gmail.com>
2025-10-15 20:56:54 -0400

    Very basic drive mode config 2

--- Core/Src/u_drive.c
+++ Core/Src/u_drive.c
@@ -11,13 +11,11 @@
 /* =================================== */
 
 /* Performance Limits */
-#define PIT_MAX_SPEED           5.0 // (mph). Speed limit in pit mode.
 #define TORQUE_ACCUMULATOR_SIZE 10  // (Number). Size of the moving average filter for torque stuff.
 #define MAX_REGEN_CURRENT       250 // (AC Amps). Maximum regenerative braking current.
 #define REGEN_INCREMENT_STEP    10  // (AC Amps). Step size for increasing/decreasing regenerative braking current.
 
 /* Drive Mode Thresholds */
-#define ACCELERATION_THRESHOLD 0.25 // (Percentage). Pedal position above which acceleration begins.
 #define REGEN_THRESHOLD 0.10        // (Percentage). Pedal position below which regenerative braking activates.
 
 /* BSPD (Brake System Plausibility Device) */
@@ -29,7 +27,7 @@
  * @param percentage_brake Brake pressure sensor reading, 0-1
  * @return bool True for prefault conditions met, false for no prefault.
  */
-static bool _calc_bspd_prefault(float percentage_accel, float percentage_brake, float dc_current)
+static bool _calc_bspd_prefault(float percentage_accel, float percentage_brake)
 {
 	static bool motor_disabled = false;
 
@@ -42,6 +40,7 @@ static bool _calc_bspd_prefault(float percentage_accel, float percentage_brake, 
 	}
 
 	/* Prevent a fault. */
+	float dc_current = dti_get_dc_current();
 	if (percentage_brake > PEDAL_HARD_BRAKE_THRESH && dc_current > 10) {
 		motor_disabled = true;
 		queue_send(&faults, &(fault_t){BSPD_PREFAULT});
@@ -61,10 +60,53 @@ static bool _calc_bspd_prefault(float percentage_accel, float percentage_brake, 
 void drive_handlePerformance(void) {
 	/* CONFIG. */
 	const uint16_t MAX_TORQUE = 220;
+	const float ACCELERATION_THRESHOLD = 0.25; /* Pedal position above which acceleration begins. */
 	
-	/* Get target torque. */
+	/* If acceleration pedal percentage isn't above the threshold, don't accelerate. */
 	pedal_data_t pedal_data = pedals_getData();
-	uint16_t torque = _PERFORMANCE(pedal_data.acceleration_percentage) * MAX_TORQUE;
+	if (pedal_data.acceleration_percentage < ACCELERATION_THRESHOLD) {
+        dti_set_torque(0);
+        return;
+    }
+
+	/* Calculate a scaled acceleration percentage from the threshold. */
+    float scaled = (pedal_data.acceleration_percentage - ACCELERATION_THRESHOLD) / (1.0f - ACCELERATION_THRESHOLD);
+	uint16_t torque = _PERFORMANCE(scaled) * MAX_TORQUE;
+
+	if(_calc_bspd_prefault(pedal_data.acceleration_percentage, pedal_data.brake_percentage)) {
+		/* Prefault triggered */
+		// dti_set_torque(0);
+		// osDelay(delay_time);
+		// return;
+	}
+
+	dti_set_torque(torque);
+}
+
+/* Handle torque when the car is in EFFICIENCY. */
+#define _EFFICIENCY(x) 1*x /* Relationship between acceleration pedal percentage and torque percentage. */
+void drive_handleEfficiency(void) {
+	/* CONFIG. */
+	const uint16_t MAX_TORQUE = 220;
+	const float ACCELERATION_THRESHOLD = 0.25; /* Pedal position above which acceleration begins. */
+	
+	/* If acceleration pedal percentage isn't above the threshold, don't accelerate. */
+	pedal_data_t pedal_data = pedals_getData();
+	if (pedal_data.acceleration_percentage < ACCELERATION_THRESHOLD) {
+        dti_set_torque(0);
+        return;
+    }
+
+	/* Calculate a scaled acceleration percentage from the threshold. */
+    float scaled = (pedal_data.acceleration_percentage - ACCELERATION_THRESHOLD) / (1.0f - ACCELERATION_THRESHOLD);
+	uint16_t torque = _EFFICIENCY(scaled) * MAX_TORQUE;
+
+	if(_calc_bspd_prefault(pedal_data.acceleration_percentage, pedal_data.brake_percentage)) {
+		/* Prefault triggered */
+		// dti_set_torque(0);
+		// osDelay(delay_time);
+		// return;
+	}
 
 	dti_set_torque(torque);
 }
@@ -75,13 +117,30 @@ void drive_handlePit(void) {
 	/* CONFIG. */
 	const uint16_t MAX_TORQUE = 10;
 	const uint16_t MAX_MPH = 5;
+	const float ACCELERATION_THRESHOLD = 0.25; /* Pedal position above which acceleration begins. */
 
-	/* Get target torque. */
-	pedal_data_t pedal_data = pedals_getData();
-	uint16_t torque = _PIT(pedal_data.acceleration_percentage) * MAX_TORQUE;
-
+	/* If we're going too fast, set the torque to zero and return. */
 	if(dti_get_mph() > MAX_MPH) {
-		torque = 0;
+		dti_set_torque(0);
+		return;
+	}
+
+	/* If acceleration pedal percentage isn't above the threshold, don't accelerate. */
+	pedal_data_t pedal_data = pedals_getData();
+	if (pedal_data.acceleration_percentage < ACCELERATION_THRESHOLD) {
+        dti_set_torque(0);
+        return;
+    }
+
+	/* Calculate a scaled acceleration percentage from the threshold. */
+    float scaled = (pedal_data.acceleration_percentage - ACCELERATION_THRESHOLD) / (1.0f - ACCELERATION_THRESHOLD);
+	uint16_t torque = _PIT(scaled) * MAX_TORQUE;
+
+	if(_calc_bspd_prefault(pedal_data.acceleration_percentage, pedal_data.brake_percentage)) {
+		/* Prefault triggered */
+		// dti_set_torque(0);
+		// osDelay(delay_time);
+		// return;
 	}
 
 	dti_set_torque(torque);
@@ -93,13 +152,30 @@ void drive_handleReverse(void) {
 	/* CONFIG. */
 	const uint16_t MAX_TORQUE = 10;
 	const uint16_t MAX_MPH = 5;
+	const float ACCELERATION_THRESHOLD = 0.25; /* Pedal position above which acceleration begins. */
 
-	/* Get target torque. */
-	pedal_data_t pedal_data = pedals_getData();
-	uint16_t torque = _REVERSE(pedal_data.acceleration_percentage) * MAX_TORQUE;
-
+	/* If we're going too fast, set the torque to zero and return. */
 	if(dti_get_mph() > MAX_MPH) {
-		torque = 0;
+		dti_set_torque(0);
+		return;
+	}
+
+	/* If acceleration pedal percentage isn't above the threshold, don't accelerate. */
+	pedal_data_t pedal_data = pedals_getData();
+	if (pedal_data.acceleration_percentage < ACCELERATION_THRESHOLD) {
+        dti_set_torque(0);
+        return;
+    }
+
+	/* Calculate a scaled acceleration percentage from the threshold. */
+    float scaled = (pedal_data.acceleration_percentage - ACCELERATION_THRESHOLD) / (1.0f - ACCELERATION_THRESHOLD);
+	uint16_t torque = _REVERSE(scaled) * MAX_TORQUE;
+
+	if(_calc_bspd_prefault(pedal_data.acceleration_percentage, pedal_data.brake_percentage)) {
+		/* Prefault triggered */
+		// dti_set_torque(0);
+		// osDelay(delay_time);
+		// return;
 	}
 
 	dti_set_torque(-1*torque);
