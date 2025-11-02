39c06976ac4ca8fd6cea13d3b5ab0bf102de6f64
Daniel Nakhooda <dnakhooda@gmail.com>
2025-11-02 18:38:34 -0500

    fix

--- Core/Src/u_lightning.c
+++ Core/Src/u_lightning.c
@@ -2,11 +2,6 @@
 #include "u_queues.h"
 #include "u_can.h"
 
-/**
- * @brief sends the given light status to the LightningBoard
- *
- * @param status the desired light status (ex: LIGHT_OFF, LIGHT_RED, LIGHT_GREEN)
- */
 void send_lightning_board_light_status(Lightning_Board_Light_Status status) {
     can_msg_t can_msg = { .id = LIGHT_BOARD_CAN_MSG_ID, .len = 2, .data = { 0 } };
     can_msg.data[0] = status;
