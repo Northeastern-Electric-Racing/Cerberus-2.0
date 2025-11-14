b744c33393d950a4950a5aac6566a889f2d6fc3d
Caio <caiodasilva05@outlook.com>
2025-11-14 08:14:46 -0500

    initial setup

---
+++ Tests/Inc/test_pedals.h
@@ -0,0 +1,6 @@
+#ifndef TEST_PEDALS_H
+#define TEST_PEDALS_H
+
+#include "unity.h"
+
+#endif
---
+++ Tests/Src/test_pedals.c
@@ -0,0 +1,21 @@
+
+#include "test_pedals.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_pedal_fault(void) {
+    
+}
+
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_pedal_fault);
+    return UNITY_END();
+}
