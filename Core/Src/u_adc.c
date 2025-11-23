83abe5a15d0234d16774efc36a974820b94cc763
Blake Jackson <blakejackson312006@gmail.com>
2025-11-23 17:03:24 -0500

    Updated CubeMX and u_adc.c for new ADC config (#96)

--- Core/Src/u_adc.c
+++ Core/Src/u_adc.c
@@ -9,17 +9,15 @@
 typedef enum {
     /* ADC Input Channel Ranks. */
     /* The order these values MUST match how the ADC ranks are set up in CubeMX. */
-    ADC1_CHANNEL0,  // SEL1
-    ADC1_CHANNEL2,  // APPS_1_ADC
     ADC1_CHANNEL3,  // EF_DASH_ADC
-    ADC1_CHANNEL5,  // SEL3
-    ADC1_CHANNEL6,  // APPS_2_ADC
-    ADC1_CHANNEL9,  // SEL4
-    ADC1_CHANNEL10, // EF_FANBATT_ADC
-    ADC1_CHANNEL12, // EF_PUMP1_ADC
+    ADC1_CHANNEL0,  // Mux 1
+    ADC1_CHANNEL5,  // Mux 3
+    ADC1_CHANNEL9,  // Mux 4
+    ADC1_CHANNEL6,  // EF_FANBATT_ADC
+    ADC1_CHANNEL2,  // EF_PUMP1_ADC
     ADC1_CHANNEL13, // EF_PUMP2_ADC
-    ADC1_CHANNEL15, // SEL2
     ADC1_CHANNEL18, // EF_MC_ADC
+    ADC1_CHANNEL15, // Mux 2
 
     /* Total number of channels/indexes for ADC1. */
     ADC1_SIZE,
@@ -30,8 +28,10 @@ static uint16_t _adc1_buffer[ADC1_SIZE]; // Buffer for the ADC DMA readings (not
 typedef enum {
     /* The order of items in this enum MUST match how the ADC ranks are set up in CubeMX. */
 
-    ADC2_CHANNEL2, // BSE_1_ADC
-    ADC2_CHANNEL6, // BSE_2_ADC
+    ADC2_CHANNEL12, // APPS_1_ADC
+    ADC2_CHANNEL10, // APPS_2_ADC
+    ADC2_CHANNEL2,  // BSE_1_ADC
+    ADC2_CHANNEL6,  // BSE_2_ADC
 
     /* Total number of indexes for ADC2. */
     ADC2_SIZE
@@ -49,12 +49,12 @@ typedef enum {
     SEL2_LOW,   // LFIU_CURRENT_2
     
     /* SEL3 */
-    SEL3_HIGH,  // SPARE_FUSE_ADC
+    SEL3_HIGH,  // LV_ADC
     SEL3_LOW,   // SHUTDOWN_ADC
 
     /* SEL4 */
     SEL4_HIGH,  // RADFAN_ADC
-    SEL4_LOW,   // SPARE1_ADC
+    SEL4_LOW,   // SPARE_FUSE_ADC
 
     /* Total number of indexes for the multiplexer buffer. */
     MUX_SIZE
@@ -134,10 +134,10 @@ raw_efuse_adc_t adc_getEFuseData(void) {
     efuses.data[EFUSE_DASHBOARD] = _adc1_buffer[ADC1_CHANNEL3];
     efuses.data[EFUSE_BRAKE] = _mux_buffer[SEL1_HIGH];
     efuses.data[EFUSE_SHUTDOWN] = _mux_buffer[SEL3_LOW];
-    efuses.data[EFUSE_LV] = 0; // ADC reading is not supported for LV eFuse.
+    efuses.data[EFUSE_LV] = _mux_buffer[SEL3_HIGH];
     efuses.data[EFUSE_RADFAN] = _mux_buffer[SEL4_HIGH];
-    efuses.data[EFUSE_FANBATT] = _adc1_buffer[ADC1_CHANNEL10];
-    efuses.data[EFUSE_PUMP1] = _adc1_buffer[ADC1_CHANNEL12];
+    efuses.data[EFUSE_FANBATT] = _adc1_buffer[ADC1_CHANNEL6];
+    efuses.data[EFUSE_PUMP1] = _adc1_buffer[ADC1_CHANNEL2];
     efuses.data[EFUSE_PUMP2] = _adc1_buffer[ADC1_CHANNEL13];
     efuses.data[EFUSE_BATTBOX] = _mux_buffer[SEL1_LOW];
     efuses.data[EFUSE_MC] = _adc1_buffer[ADC1_CHANNEL18];
@@ -151,8 +151,8 @@ raw_pedal_adc_t adc_getPedalData(void) {
     raw_pedal_adc_t sensors = { 0 };
 
     mutex_get(&adc_mutex);
-    sensors.data[PEDAL_ACCEL1] = _adc1_buffer[ADC1_CHANNEL2];
-    sensors.data[PEDAL_ACCEL2] = _adc1_buffer[ADC1_CHANNEL6];
+    sensors.data[PEDAL_ACCEL1] = _adc2_buffer[ADC2_CHANNEL12];
+    sensors.data[PEDAL_ACCEL2] = _adc2_buffer[ADC2_CHANNEL10];
     sensors.data[PEDAL_BRAKE1] = _adc2_buffer[ADC2_CHANNEL2];
     sensors.data[PEDAL_BRAKE2] = _adc2_buffer[ADC2_CHANNEL6];
     mutex_put(&adc_mutex);
