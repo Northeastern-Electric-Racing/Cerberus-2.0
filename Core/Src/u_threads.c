2a4320e0010ef3d16dd329cd2e8481f645ad76f5
Blake Jackson <blakejackson312006@gmail.com>
2025-12-27 14:29:43 -0500

    Changed peripheral CAN message formats to signed ints. (#105)

--- Core/Src/u_threads.c
+++ Core/Src/u_threads.c
@@ -459,22 +459,22 @@ void vPeripherals(ULONG thread_input) {
 
     /* Format for the temp sensor CAN data. */
     typedef struct __attribute__((__packed__)) {
-		uint16_t temperature;
+		int16_t temperature;
         uint16_t humidity;
 	} tempsensor_CAN_t;
 
     /* Format for the IMU Acceleration CAN data. */
     typedef struct __attribute__((__packed__)) {
-		uint16_t x;
-        uint16_t y;
-        uint16_t z;
+		int16_t x;
+        int16_t y;
+        int16_t z;
 	} acceleration_CAN_t;
 
     /* Format for the IMU Gyro CAN data. */
     typedef struct __attribute__((__packed__)) {
-		uint16_t x;
-        uint16_t y;
-        uint16_t z;
+		int16_t x;
+        int16_t y;
+        int16_t z;
 	} gyro_CAN_t;
     
     while(1) {
@@ -489,7 +489,7 @@ void vPeripherals(ULONG thread_input) {
 
         /* Fill the temp sensor message and send it over CAN. */
         tempsensor_CAN_t tempsensor_CAN = { 0 };
-        tempsensor_CAN.temperature = (uint16_t)(temperature * 100);
+        tempsensor_CAN.temperature = (int16_t)(temperature * 100);
         tempsensor_CAN.humidity = (uint16_t)(humidity * 100);
         can_msg_t temp_sensor_message = {.id = CANID_TEMP_SENSOR, .len = 4, .id_is_extended = false};
         memcpy(temp_sensor_message.data, &tempsensor_CAN, temp_sensor_message.len);
@@ -504,9 +504,9 @@ void vPeripherals(ULONG thread_input) {
 
         /* Fill the IMU acceleration message and send it over CAN. */
         acceleration_CAN_t acceleration_CAN = { 0 };
-        acceleration_CAN.x = (uint16_t)(acceleration.x * 100);
-        acceleration_CAN.y = (uint16_t)(acceleration.y * 100);
-        acceleration_CAN.z = (uint16_t)(acceleration.z * 100);
+        acceleration_CAN.x = (int16_t)(acceleration.x * 100);
+        acceleration_CAN.y = (int16_t)(acceleration.y * 100);
+        acceleration_CAN.z = (int16_t)(acceleration.z * 100);
         can_msg_t imu_acceleration_message = {.id = CANID_IMU_ACCEL, .len = 6, .id_is_extended = false};
         memcpy(imu_acceleration_message.data, &acceleration_CAN, imu_acceleration_message.len);
         queue_send(&can_outgoing, &imu_acceleration_message, TX_NO_WAIT);
@@ -520,9 +520,9 @@ void vPeripherals(ULONG thread_input) {
 
         /* Fill the IMU Gyro message and send it over CAN. */
         gyro_CAN_t gyro_CAN = { 0 };
-        gyro_CAN.x = (uint16_t)(gyro.x * 100);
-        gyro_CAN.y = (uint16_t)(gyro.y * 100);
-        gyro_CAN.z = (uint16_t)(gyro.z * 100);
+        gyro_CAN.x = (int16_t)(gyro.x * 100);
+        gyro_CAN.y = (int16_t)(gyro.y * 100);
+        gyro_CAN.z = (int16_t)(gyro.z * 100);
         can_msg_t imu_gyro_message = {.id = CANID_IMU_GYRO, .len = 6, .id_is_extended = false};
         memcpy(imu_gyro_message.data, &gyro_CAN, imu_gyro_message.len);
         queue_send(&can_outgoing, &imu_gyro_message, TX_NO_WAIT);
