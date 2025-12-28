08f82b0e791f9a5fbe2218084def6b78cdbd92a4
bjackson312006 <blakejackson312006@gmail.com>
2025-12-28 14:19:34 -0500

    Added error checking for mutex_get() and mutex_put() in u_peripherals.c

--- Core/Src/u_peripherals.c
+++ Core/Src/u_peripherals.c
@@ -162,9 +162,9 @@ int peripherals_init(void) {
 
 /* Toggles the status of the temperature sensor's internal heater. */
 int tempsensor_toggleHeater(bool enable) {
-    mutex_get(&peripherals_mutex);
+    CATCH_ERROR(mutex_get(&peripherals_mutex), U_SUCCESS);
     HAL_StatusTypeDef status = sht30_toggle_heater(&temperature_sensor, enable);
-    mutex_put(&peripherals_mutex);
+    CATCH_ERROR(mutex_put(&peripherals_mutex), U_SUCCESS);
     if(status != HAL_OK) {
         PRINTLN_ERROR("Failed to toggle SHT30 heater (Status: %d/%s).", status, hal_status_toString(status));
         return U_ERROR;
@@ -174,9 +174,9 @@ int tempsensor_toggleHeater(bool enable) {
 
 /* Gets the temp sensor's temperature and humidity readings. */
 int tempsensor_getTemperatureAndHumidity(float *temperature, float *humidity) {
-    mutex_get(&peripherals_mutex);
+    CATCH_ERROR(mutex_get(&peripherals_mutex), U_SUCCESS)
     int status = sht30_get_temp_humid(&temperature_sensor);
-    mutex_put(&peripherals_mutex);
+    CATCH_ERROR(mutex_put(&peripherals_mutex), U_SUCCESS);
     if(status != 0) {
         PRINTLN_ERROR("Failed to read SHT30 temperature/humidity (Status: %d).", status);
         return U_ERROR;
@@ -192,9 +192,9 @@ int imu_getAcceleration(vector3_t* data) {
     int16_t raw_data[3];
     
     /* Read raw accelerometer data. */
-    mutex_get(&peripherals_mutex);
+    CATCH_ERROR(mutex_get(&peripherals_mutex), U_SUCCESS);
     int status = lsm6dsv_acceleration_raw_get(&imu, raw_data);
-    mutex_put(&peripherals_mutex);
+    CATCH_ERROR(mutex_put(&peripherals_mutex), U_SUCCESS);
     if(status != 0) {
         PRINTLN_ERROR("Failed to read accelerometer data.");
         return U_ERROR;
@@ -213,9 +213,9 @@ int imu_getAngularRate(vector3_t* data) {
     int16_t raw_data[3];
     
     /* Read raw gyroscope data. */
-    mutex_get(&peripherals_mutex);      // u_TODO - All of these functions have to get the peripherals mutex. Since these funtions are (as of rn) being called on-after-another by the peripherals thread, this is inefficient (ideally, the mutex would just need to aquired once and put back once). It could be a good idea to have a convention where it's the caller's responsibility to acquire the mutex for these sorts of functions, and then have some kind of check in this funciton to make sure the mutex is owned by the caller's thread
+    CATCH_ERROR(mutex_get(&peripherals_mutex), U_SUCCESS);
     int status = lsm6dsv_angular_rate_raw_get(&imu, raw_data);
-    mutex_put(&peripherals_mutex);
+    CATCH_ERROR(mutex_put(&peripherals_mutex), U_SUCCESS);
     if(status != 0) {
         PRINTLN_ERROR("Failed to read gyroscope data.");
         return U_ERROR;
