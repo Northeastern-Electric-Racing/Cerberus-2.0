552f387bf6c2717b2a8d80d1140dbcca9e95021e
bjackson312006 <blakejackson312006@gmail.com>
2025-12-28 19:42:13 -0500

    Removed ADC mutex

--- Core/Inc/u_mutexes.h
+++ Core/Inc/u_mutexes.h
@@ -10,7 +10,6 @@
 /* Mutex List */
 extern mutex_t bms_mutex;          // BMS Mutex
 extern mutex_t dti_mutex;          // DTI Mutex
-extern mutex_t adc_mutex;          // ADC Mutex
 extern mutex_t peripherals_mutex;  // Peripherals Mutex
 // add more as necessary...
 
--- Core/Src/u_mutexes.c
+++ Core/Src/u_mutexes.c
@@ -14,12 +14,6 @@ mutex_t dti_mutex = {
     .priority_inherit = TX_INHERIT /* Priority inheritance setting. */
 };
 
-/* ADC Mutex */
-mutex_t adc_mutex = {
-    .name = "ADC Mutex",           /* Name of the mutex. */
-    .priority_inherit = TX_INHERIT /* Priority inheritance setting. */
-};
-
 /* Peripherals Mutex */
 mutex_t peripherals_mutex = {
     .name = "Peripherals Mutex",   /* Name of the mutex. */
@@ -33,7 +27,6 @@ uint8_t mutexes_init() {
     /* Create Mutexes. */
     CATCH_ERROR(create_mutex(&bms_mutex), U_SUCCESS);          // Create BMS Mutex.
     CATCH_ERROR(create_mutex(&dti_mutex), U_SUCCESS);          // Create DTI Mutex.
-    CATCH_ERROR(create_mutex(&adc_mutex), U_SUCCESS);          // Create ADC Mutex.
     CATCH_ERROR(create_mutex(&peripherals_mutex), U_SUCCESS);  // Create Peripherals Mutex.
 
     // add more as necessary.
