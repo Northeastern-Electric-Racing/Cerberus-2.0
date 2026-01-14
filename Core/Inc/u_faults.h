45631a13f476eeada22409624c43a46d7ce2b8fd
bjackson312006 <blakejackson312006@gmail.com>
2026-01-14 18:55:12 -0500

    added new get_fault() function to get the state of a specific fault

--- Core/Inc/u_faults.h
+++ Core/Inc/u_faults.h
@@ -31,6 +31,7 @@ typedef enum {
 int faults_init(void);
 int trigger_fault(fault_t fault_id);
 uint32_t get_faults(void);
+bool get_fault(fault_t fault);	   /* Returns whether or not a specific fault is active. */
 void write_mcu_fault(bool status); /* Write the VCU FAULT line (from the microcontroller to the car). true = faulted, false = unfaulted. */
 
 #endif /* u_faults.h */
