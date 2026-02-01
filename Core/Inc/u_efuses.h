3e2ad3711a2e8dab9d1eee6548a413a3b845fcb6
sogo <sogonishi@gmail.com>
2026-02-01 18:44:11 -0500

    clean up codes in efuse thread's main loop

--- Core/Inc/u_efuses.h
+++ Core/Inc/u_efuses.h
@@ -31,8 +31,41 @@ typedef struct {
 } efuse_data_t;
 
 /* API */
-efuse_data_t efuse_getData(void);  // Returns an instance of efuse_data_t with all current eFuse data.
 void efuse_enable(efuse_t efuse);  // Enables an eFuse.
 void efuse_disable(efuse_t efuse); // Disables an eFuse.
+
+
+/**
+ * @brief Enables eFuses which should be on by default.
+ * 
+ * The following eFuses should be enabled by default:
+ * - `EFUSE_DASHBOARD`
+ * - `EFUSE_LV`
+ * - `EFUSE_BATTBOX`
+ * - `EFUSE_MC`
+ * - `EFUSE_FANBATT`  ?? (We may need to do this conditionally based on pack temp)
+ */
+void efuse_init(void);
+
+/**
+ * @brief Updates eFuses' states.
+ * 
+ * The following eFuses should be turned on:
+ * - `EFUSE_PUMP1`
+ *      - Should be turned on if motor is higher than X temp.
+ *      - Otherwise, turned off.
+ * - `EFUSE_PUMP2`
+ *      - Should be turned on if MC is higher than Y temp.
+ *      - Otherwise, turned off.
+ * - `EFUSE_RADFAN`
+ *      - Should be turned on if motor is higher than Z temp.
+ *      - Otherwise, turned off.
+ * 
+ * Also, some eFuses should be updated according to the CAN message.
+ */
+void efuse_update(void);
+
+void efuse_send_to_dashboard(void);
+void efuse_send_to_serial(void);
 
 #endif /* u_efuses.h */
