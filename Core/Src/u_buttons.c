f5ba5eac955fb0223df33191f54300301f4b8e9c
bjackson312006 <blakejackson312006@gmail.com>
2026-02-08 15:38:51 -0500

    added the functions to source file

--- Core/Src/u_buttons.c
+++ Core/Src/u_buttons.c
@@ -1,3 +1,41 @@
 #include "u_buttons.h"
 
-void button1_onPressed(void);
+void button1_onPressed(void) {
+    return;
+}
+
+void button2_onPressed(void) {
+    return;
+}
+
+void button3_onPressed(void) {
+    return;
+}
+
+void button4_onPressed(void) {
+    return;
+}
+
+void button5_onPressed(void) {
+    return;
+}
+
+void button6_onPressed(void) {
+    return;
+}
+
+void button7_onPressed(void) {
+    return;
+}
+
+void button8_onPressed(void) {
+    return;
+}
+
+void button9_onPressed(void) {
+    return;
+}
+
+void button10_onPressed(void) {
+    return;
+}
