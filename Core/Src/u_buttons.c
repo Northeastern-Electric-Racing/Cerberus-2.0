12b70e3835877c518931fc6772b824e6aa5b3d42
bjackson312006 <blakejackson312006@gmail.com>
2026-02-08 15:50:35 -0500

    added PRINTLN_INFO() messages for each button press

--- Core/Src/u_buttons.c
+++ Core/Src/u_buttons.c
@@ -1,41 +1,52 @@
 #include "u_buttons.h"
+#include "u_tx_debug.h"
 
 void button1_onPressed(void) {
+    PRINTLN_INFO("Button 1 pressed.");
     return;
 }
 
 void button2_onPressed(void) {
+    PRINTLN_INFO("Button 2 pressed.");
     return;
 }
 
 void button3_onPressed(void) {
+    PRINTLN_INFO("Button 3 pressed.");
     return;
 }
 
 void button4_onPressed(void) {
+    PRINTLN_INFO("Button 4 pressed.");
     return;
 }
 
 void button5_onPressed(void) {
+    PRINTLN_INFO("Button 5 pressed.");
     return;
 }
 
 void button6_onPressed(void) {
+    PRINTLN_INFO("Button 6 pressed.");
     return;
 }
 
 void button7_onPressed(void) {
+    PRINTLN_INFO("Button 7 pressed.");
     return;
 }
 
 void button8_onPressed(void) {
+    PRINTLN_INFO("Button 8 pressed.");
     return;
 }
 
 void button9_onPressed(void) {
+    PRINTLN_INFO("Button 9 pressed.");
     return;
 }
 
 void button10_onPressed(void) {
+    PRINTLN_INFO("Button 10 pressed.");
     return;
 }
