f4ab80b28f74a9035c9629ff879a1824f6e22d35
Surya Thoppae <63197592+suryatho@users.noreply.github.com>
2026-02-09 20:52:17 -0500

    Fix merge oops

--- Core/Src/u_lightning.c
+++ Core/Src/u_lightning.c
@@ -10,10 +10,13 @@
 #define LIGHTNING_CAN_MONITOR_DELAY 4000
 
 /* Fault callback(s). */
-static void _bms_fault_callback(ULONG args) { queue_send(&faults, &(fault_t){LIGHTNING_CAN_MONITOR_FAULT}, TX_NO_WAIT); }; // Queues the BMS CAN Monitor Fault.
+static void _lightning_fault_callback(ULONG args) {
+    queue_send(&faults, &(fault_t){LIGHTNING_CAN_MONITOR_FAULT}, TX_NO_WAIT);
+}
+// Queues the BMS CAN Monitor Fault.
 static timer_t lightning_fault_timer = {
     .name = "Lightning Fault Timer",
-    .callback = _bms_fault_callback,
+    .callback = _lightning_fault_callback,
     .callback_input = 0,
     .duration = LIGHTNING_CAN_MONITOR_DELAY,
     .type = ONESHOT,
@@ -22,10 +25,10 @@ static timer_t lightning_fault_timer = {
 
 /* Initializes the lightning fault timer. */
 int lightning_init(void) {
-    /* Create BMS Timer. */
+    /* Create Lightning Timer. */
     int status = timer_init(&lightning_fault_timer);
     if (status != U_SUCCESS) {
-        PRINTLN_ERROR("Failed to create BMS Fault Timer (Status: %d).", status);
+        PRINTLN_ERROR("Failed to create lightning Fault Timer (Status: %d).", status);
         return U_ERROR;
     }
 
