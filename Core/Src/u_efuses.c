1d9bdc6a094244db740d2397aafac3f269ccbaee
bjackson312006 <blakejackson312006@gmail.com>
2026-02-22 15:09:06 -0500

    possibly fixed efuse scaling values

--- Core/Src/u_efuses.c
+++ Core/Src/u_efuses.c
@@ -23,15 +23,15 @@ typedef struct {
 /* EFuse Table. */
 /* This table should be kept in the same order as the efuse_t enum in the header file. */
 static const _metadata efuses[] = {
-    [EFUSE_DASHBOARD] = {.en_pin = EF_DASH_EN_Pin, .en_port = EF_DASH_EN_GPIO_Port, .er_pin = EF_DASH_ER_Pin, .er_port = EF_DASH_ER_GPIO_Port, .scale = SCALE(110)},
+    [EFUSE_DASHBOARD] = {.en_pin = EF_DASH_EN_Pin, .en_port = EF_DASH_EN_GPIO_Port, .er_pin = EF_DASH_ER_Pin, .er_port = EF_DASH_ER_GPIO_Port, .scale = SCALE(39)},
     [EFUSE_BRAKE] = {.en_pin = EF_BREAK_EN_Pin, .en_port = EF_BREAK_EN_GPIO_Port, .er_pin = EF_BREAK_ER_Pin, .er_port = EF_BREAK_ER_GPIO_Port, .scale = SCALE(200)},
     [EFUSE_SHUTDOWN] = {.en_pin = EF_SHUTDOWN_EN_Pin, .en_port = EF_SHUTDOWN_EN_GPIO_Port, .er_pin = EF_SHUTDOWN_ER_Pin, .er_port = EF_SHUTDOWN_ER_GPIO_Port, .scale = SCALE(110)},
-    [EFUSE_LV] = {.en_pin = EF_LV_EN_Pin, .en_port = EF_LV_EN_GPIO_Port, .er_pin = EF_LV_ER_Pin, .er_port = EF_LV_ER_GPIO_Port, .scale = SCALE(75)},
+    [EFUSE_LV] = {.en_pin = EF_LV_EN_Pin, .en_port = EF_LV_EN_GPIO_Port, .er_pin = EF_LV_ER_Pin, .er_port = EF_LV_ER_GPIO_Port, .scale = SCALE(39)},
     [EFUSE_RADFAN] = {.en_pin = EF_RADFAN_EN_Pin, .en_port = EF_RADFAN_EN_GPIO_Port, .er_pin = EF_RADFAN_ER_Pin, .er_port = EF_RADFAN_ER_GPIO_Port, .scale = SCALE(56)},
-    [EFUSE_FANBATT] = {.en_pin = EF_FANBATT_EN_Pin, .en_port = EF_FANBATT_EN_GPIO_Port, .er_pin = EF_FANBATT_ER_Pin, .er_port = EF_FANBATT_ER_GPIO_Port, .scale = SCALE(39)},
+    [EFUSE_FANBATT] = {.en_pin = EF_FANBATT_EN_Pin, .en_port = EF_FANBATT_EN_GPIO_Port, .er_pin = EF_FANBATT_ER_Pin, .er_port = EF_FANBATT_ER_GPIO_Port, .scale = SCALE(27)},
     [EFUSE_PUMP1] = {.en_pin = EF_PUMP1_EN_Pin, .en_port = EF_PUMP1_EN_GPIO_Port, .er_pin = EF_PUMP1_ER_Pin, .er_port = EF_PUMP1_ER_GPIO_Port, .scale = SCALE(47)},
     [EFUSE_PUMP2] = {.en_pin = EF_PUMP2_EN_Pin, .en_port = EF_PUMP2_EN_GPIO_Port, .er_pin = EF_PUMP2_ER_Pin, .er_port = EF_PUMP2_ER_GPIO_Port, .scale = SCALE(47)},
-    [EFUSE_BATTBOX] = {.en_pin = EF_BATTBOX_EN_Pin, .en_port = EF_BATTBOX_EN_GPIO_Port, .er_pin = EF_BATTBOX_ER_Pin, .er_port = EF_BATTBOX_ER_GPIO_Port, .scale = SCALE(39)},
+    [EFUSE_BATTBOX] = {.en_pin = EF_BATTBOX_EN_Pin, .en_port = EF_BATTBOX_EN_GPIO_Port, .er_pin = EF_BATTBOX_ER_Pin, .er_port = EF_BATTBOX_ER_GPIO_Port, .scale = SCALE(56)},
     [EFUSE_MC] = {.en_pin = EF_MC_EN_Pin, .en_port = EF_MC_EN_GPIO_Port, .er_pin = EF_MC_ER_Pin, .er_port = EF_MC_ER_GPIO_Port, .scale = SCALE(56)}
 };
 
--- Core/Src/u_threads.c
+++ Core/Src/u_threads.c
@@ -779,9 +779,9 @@ uint8_t threads_init(TX_BYTE_POOL *byte_pool) {
     //CATCH_ERROR(create_thread(byte_pool, &statemachine_thread), U_SUCCESS);      // Create State Machine thread.
     //CATCH_ERROR(create_thread(byte_pool, &pedals_thread), U_SUCCESS);            // Create Pedals thread.
     CATCH_ERROR(create_thread(byte_pool, &efuses_thread), U_SUCCESS);              // Create eFuses thread.
-    //CATCH_ERROR(create_thread(byte_pool, &mux_thread), U_SUCCESS);               // Create Mux thread.
+    CATCH_ERROR(create_thread(byte_pool, &mux_thread), U_SUCCESS);               // Create Mux thread.
     CATCH_ERROR(create_thread(byte_pool, &peripherals_thread), U_SUCCESS);       // Create Peripherals thread.
-    //CATCH_ERROR(create_thread(byte_pool, &ethernet_incoming_thread), U_SUCCESS); // Create Incoming Ethernet thread.
+    CATCH_ERROR(create_thread(byte_pool, &ethernet_incoming_thread), U_SUCCESS); // Create Incoming Ethernet thread.
     CATCH_ERROR(create_thread(byte_pool, &ethernet_outgoing_thread), U_SUCCESS); // Create Outgoing Ethernet thread.
     CATCH_ERROR(create_thread(byte_pool, &test_thread), U_SUCCESS);                // Create Test thread.
 
