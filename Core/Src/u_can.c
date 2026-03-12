fc828df61567094d85cf93e14fd91ab24283f278
bjackson312006 <blakejackson312006@gmail.com>
2026-03-12 18:13:53 -0400

    Added CANIDs and filters for the calypso efuse command messages

--- Core/Src/u_can.c
+++ Core/Src/u_can.c
@@ -43,10 +43,42 @@ uint8_t can1_init(FDCAN_HandleTypeDef *hcan) {
     }
 
     /* Add fitlers for extended IDs */
-    uint32_t extended[] = {0x00, 0x00};
-    status = can_add_filter_extended(&can1, extended);
+    uint32_t extended1[] = {CANID_CALYPSO_EFCTRL_DASHBOARD, CANID_CALYPSO_EFCTRL_BRAKE};
+    status = can_add_filter_extended(&can1, extended1);
     if (status != HAL_OK) {
-        PRINTLN_ERROR("Failed to add extended filter to can1 (Status: %d/%s, ID1: %ld, ID2: %ld).", status, hal_status_toString(status), extended[0], extended[1]);
+        PRINTLN_ERROR("Failed to add extended filter to can1 (Status: %d/%s, ID1: %ld, ID2: %ld).", status, hal_status_toString(status), extended1[0], extended1[1]);
+        return U_ERROR;
+    }
+
+    /* Add fitlers for extended IDs */
+    uint32_t extended2[] = {CANID_CALYPSO_EFCTRL_SHUTDOWN, CANID_CALYPSO_EFCTRL_LV};
+    status = can_add_filter_extended(&can1, extended2);
+    if (status != HAL_OK) {
+        PRINTLN_ERROR("Failed to add extended filter to can1 (Status: %d/%s, ID1: %ld, ID2: %ld).", status, hal_status_toString(status), extended2[0], extended2[1]);
+        return U_ERROR;
+    }
+
+    /* Add fitlers for extended IDs */
+    uint32_t extended3[] = {CANID_CALYPSO_EFCTRL_RADFAN, CANID_CALYPSO_EFCTRL_FANBATT};
+    status = can_add_filter_extended(&can1, extended3);
+    if (status != HAL_OK) {
+        PRINTLN_ERROR("Failed to add extended filter to can1 (Status: %d/%s, ID1: %ld, ID2: %ld).", status, hal_status_toString(status), extended3[0], extended3[1]);
+        return U_ERROR;
+    }
+
+    /* Add fitlers for extended IDs */
+    uint32_t extended4[] = {CANID_CALYPSO_EFCTRL_PUMPONE, CANID_CALYPSO_EFCTRL_PUMPTWO};
+    status = can_add_filter_extended(&can1, extended4);
+    if (status != HAL_OK) {
+        PRINTLN_ERROR("Failed to add extended filter to can1 (Status: %d/%s, ID1: %ld, ID2: %ld).", status, hal_status_toString(status), extended4[0], extended4[1]);
+        return U_ERROR;
+    }
+
+    /* Add fitlers for extended IDs */
+    uint32_t extended5[] = {CANID_CALYPSO_EFCTRL_BATTBOX, CANID_CALYPSO_EFCTRL_MC};
+    status = can_add_filter_extended(&can1, extended5);
+    if (status != HAL_OK) {
+        PRINTLN_ERROR("Failed to add extended filter to can1 (Status: %d/%s, ID1: %ld, ID2: %ld).", status, hal_status_toString(status), extended5[0], extended5[1]);
         return U_ERROR;
     }
 
