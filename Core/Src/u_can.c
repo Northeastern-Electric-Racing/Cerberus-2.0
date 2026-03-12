8022cff70be024612f66a940725a20c502de10f5
bjackson312006 <blakejackson312006@gmail.com>
2026-03-12 12:44:03 -0400

    VCU recieves DTI messages. Also added all messages currently in can_inbox() to the filter, since that was not done yet.

--- Core/Src/u_can.c
+++ Core/Src/u_can.c
@@ -18,10 +18,26 @@ uint8_t can1_init(FDCAN_HandleTypeDef *hcan) {
     }
 
     /* Add filters for standard IDs */
-    uint16_t standard[] = {0x00, 0x00};
+    uint16_t standard[] = {CANID_BMS_DCL_MSG, CANID_BMS_CELL_TEMPS};
     status = can_add_filter_standard(&can1, standard);
     if (status != HAL_OK) {
-        PRINTLN_ERROR("Failed to add standard filter to can1 (Status: %d/%s, ID1: %d, ID2: %d).", status, hal_status_toString(status), standard[0], standard[1]);
+        PRINTLN_ERROR("Failed to add standard filter to can1 (Status: %d/%s, ID1: 0x%X, ID2: 0x%X).", status, hal_status_toString(status), standard[0], standard[1]);
+        return U_ERROR;
+    }
+
+    /* Add filters for standard IDs */
+    standard[] = {IMU_CAN_MSG_ID, DTI_CANID_TEMPS_FAULT};
+    status = can_add_filter_standard(&can1, standard);
+    if (status != HAL_OK) {
+        PRINTLN_ERROR("Failed to add standard filter to can1 (Status: %d/%s, ID1: 0x%X, ID2: 0x%X).", status, hal_status_toString(status), standard[0], standard[1]);
+        return U_ERROR;
+    }
+
+    /* Add filters for standard IDs */
+    standard[] = {DTI_CANID_ERPM, DTI_CANID_CURRENTS};
+    status = can_add_filter_standard(&can1, standard);
+    if (status != HAL_OK) {
+        PRINTLN_ERROR("Failed to add standard filter to can1 (Status: %d/%s, ID1: 0x%X, ID2: 0x%X).", status, hal_status_toString(status), standard[0], standard[1]);
         return U_ERROR;
     }
 
@@ -50,6 +66,15 @@ void can_inbox(can_msg_t *message) {
         break;
     case IMU_CAN_MSG_ID:
         lightning_handleIMUMessage();
+        break;
+    case DTI_CANID_TEMPS_FAULT:
+        dti_record_temp(message);
+        break;
+    case DTI_CANID_ERPM:
+        dti_record_rpm(message);
+        break;
+    case DTI_CANID_CURRENTS:
+        dti_record_currents(message);
         break;
     default:
         PRINTLN_ERROR("Unknown CAN Message Recieved (Message ID: %ld).", message->id);
