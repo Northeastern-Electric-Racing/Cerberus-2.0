e7c02a9e18b3a17cbebf09d84499ef7d6423e9f9
bjackson312006 <blakejackson312006@gmail.com>
2026-04-18 00:31:58 -0400

    override calypso option

--- Core/Src/u_efuses.c
+++ Core/Src/u_efuses.c
@@ -25,7 +25,7 @@ typedef struct {
 /* EFuse Table. */
 /* This table should be kept in the same order as the efuse_t enum in the header file. */
 static const _metadata efuses[] = {
-    [EFUSE_DASHBOARD] = {.en_pin = EF_DASH_EN_Pin, .en_port = EF_DASH_EN_GPIO_Port, .er_pin = EF_DASH_ER_Pin, .er_port = EF_DASH_ER_GPIO_Port, .scale = SCALE(39), .default_state = EF_ON},
+    [EFUSE_DASHBOARD] = {.en_pin = EF_DASH_EN_Pin, .en_port = EF_DASH_EN_GPIO_Port, .er_pin = EF_DASH_ER_Pin, .er_port = EF_DASH_ER_GPIO_Port, .scale = SCALE(39), .default_state = EF_OFF},
     [EFUSE_BRAKE] = {.en_pin = EF_BREAK_EN_Pin, .en_port = EF_BREAK_EN_GPIO_Port, .er_pin = EF_BREAK_ER_Pin, .er_port = EF_BREAK_ER_GPIO_Port, .scale = SCALE(200), .default_state = EF_AUTO},
     [EFUSE_SHUTDOWN] = {.en_pin = EF_SHUTDOWN_EN_Pin, .en_port = EF_SHUTDOWN_EN_GPIO_Port, .er_pin = EF_SHUTDOWN_ER_Pin, .er_port = EF_SHUTDOWN_ER_GPIO_Port, .scale = SCALE(110), .default_state = EF_ON},
     [EFUSE_LV] = {.en_pin = EF_LV_EN_Pin, .en_port = EF_LV_EN_GPIO_Port, .er_pin = EF_LV_ER_Pin, .er_port = EF_LV_ER_GPIO_Port, .scale = SCALE(39), .default_state = EF_ON},
@@ -75,7 +75,11 @@ efuse_data_t efuse_getData(void) {
         data.enabled[efuse] = (bool)(HAL_GPIO_ReadPin(efuses[efuse].en_port, efuses[efuse].en_pin) == GPIO_PIN_SET);
 
         /* Get the eFuse's current control state (as understood by VCU) from the control state tracking array. */
-        data.control_state[efuse] = _efuse_control_state[efuse];
+        #ifdef EFUSES_OVERRIDE_CALYPSO
+        data.control_state[efuse] = efuses[efuse].default_state; // If VCU is set to override Calypso, use the fuse's static default control state.
+        #else
+        data.control_state[efuse] = _efuse_control_state[efuse]; // In normal operation, source control state from Calypso.
+        #endif
     }
 
     return data;
