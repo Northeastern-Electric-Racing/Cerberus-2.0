1c5bd83604fd4899fa287a53edd4d59c1e801da3
Caio DaSilva <dasilva.cai@northeastern.edu>
2026-05-30 13:33:48 -0400

    added latching reset message

--- Core/Inc/can_messages_tx.h
+++ Core/Inc/can_messages_tx.h
@@ -10,27 +10,6 @@
 
 /**
 * Contents of this message:
-* VCU/Commands/AC_Current_Target - This command sets the target motor AC current (peak, not RMS)
-*/
-uint8_t send_ac_current_command
-(float current_target_ac);
-
-/**
-* Contents of this message:
-* VCU/Commands/Brake_Current_Target - Targets the brake current of the motor
-*/
-uint8_t send_brake_current_command
-(float brake_ac_current);
-
-/**
-* Contents of this message:
-* VCU/Commands/Drive_Enable_Target - Drive allowed
-*/
-uint8_t send_drive_enable_command
-(uint8_t drive_enable);
-
-/**
-* Contents of this message:
 * VCU/eFuses/Dashboard/ADC - Raw ADC Value.
 * VCU/eFuses/Dashboard/Voltage - Dashboard eFuse Voltage
 * VCU/eFuses/Dashboard/Current - Dashboard eFuse Current.
@@ -165,7 +144,6 @@ uint8_t send_spare_efuse
 * Contents of this message:
 * VCU/Shutdown/BMS_GPIO - 
 * VCU/Shutdown/BOTS_GPIO - 
-* VCU/Shutdown/SPARE_GPIO - 
 * VCU/Shutdown/BSPD_GPIO - 
 * VCU/Shutdown/HV_C_GPIO - 
 * VCU/Shutdown/HVD_GPIO - 
@@ -175,7 +153,7 @@ uint8_t send_spare_efuse
 * VCU/Shutdown/TSMS_GPIO - 
 */
 uint8_t send_shutdown_pins
-(bool bms_gpio,bool bots_gpio,bool spare_gpio,bool bspd_gpio,bool hv_c,bool hvd_gpio,bool imd_gpio,bool ckpt_gpio,bool inertia_sw_gpio,bool tsms_gpio);
+(bool bms_gpio,bool bots_gpio,bool bspd_gpio,bool hv_c,bool hvd_gpio,bool imd_gpio,bool ckpt_gpio,bool inertia_sw_gpio,bool tsms_gpio);
 
 /**
 * Contents of this message:
@@ -260,9 +238,10 @@ uint8_t send_imu_gyro
 * VCU/Faults/Non-Critical/RTDS_FAULT - 
 * VCU/Faults/Non-Critical/LV_LOW_VOLTAGE_FAULT - 
 * VCU/Faults/Critical/PRECHARGE_FLOATING_FAULT - If TS Volts is stuck under the Batt Volts Threshold
+* VCU/Faults/Critical/LATCHING_ACTIVE_FAULT - 
 */
 uint8_t send_faults
-(bool CAN_OUTGOING_FAULT,bool CAN_INCOMING_FAULT,bool BMS_CAN_MONITOR_FAULT,bool LIGHTNING_CAN_MONITOR_FAULT,bool ONBOARD_TEMP_FAULT,bool IMU_ACCEL_FAULT,bool IMU_GYRO_FAULT,bool BSPD_PREFAULT,bool ONBOARD_BRAKE_OPEN_CIRCUIT_FAULT,bool ONBOARD_ACCEL_OPEN_CIRCUIT_FAULT,bool ONBOARD_BRAKE_SHORT_CIRCUIT_FAULT,bool ONBOARD_ACCEL_SHORT_CIRCUIT_FAULT,bool ONBOARD_PEDAL_DIFFERENCE_FAULT,bool RTDS_FAULT,bool LV_LOW_VOLTAGE_FAULT,bool PRECHARGE_FLOATING_FAULT);
+(bool CAN_OUTGOING_FAULT,bool CAN_INCOMING_FAULT,bool BMS_CAN_MONITOR_FAULT,bool LIGHTNING_CAN_MONITOR_FAULT,bool ONBOARD_TEMP_FAULT,bool IMU_ACCEL_FAULT,bool IMU_GYRO_FAULT,bool BSPD_PREFAULT,bool ONBOARD_BRAKE_OPEN_CIRCUIT_FAULT,bool ONBOARD_ACCEL_OPEN_CIRCUIT_FAULT,bool ONBOARD_BRAKE_SHORT_CIRCUIT_FAULT,bool ONBOARD_ACCEL_SHORT_CIRCUIT_FAULT,bool ONBOARD_PEDAL_DIFFERENCE_FAULT,bool RTDS_FAULT,bool LV_LOW_VOLTAGE_FAULT,bool PRECHARGE_FLOATING_FAULT,bool LATCHING_ACTIVE_FAULT);
 
 /**
 * Contents of this message:
@@ -377,4 +356,32 @@ uint8_t send_bms_shutdown_status_as_reported_by_vcu
 */
 uint8_t send_drive_lock_states
 (bool BRAKE_OC,bool BRAKE_SC,bool ACCEL_OC,bool ACCEL_SC,bool ACCEL_DIFF,bool BSPD_PREF,bool BMS_NOT_PRECHARGED_YET);
+
+/**
+* Contents of this message:
+* VCU/Lightning/Reset_Latching_Faults - Tells Lightning to Reset Latching Faults
+*/
+uint8_t send_reset_latching_fault
+(bool reset_latching);
+
+/**
+* Contents of this message:
+* VCU/Commands/AC_Current_Target - This command sets the target motor AC current (peak, not RMS)
+*/
+uint8_t send_ac_current_command
+(float current_target_ac);
+
+/**
+* Contents of this message:
+* VCU/Commands/Brake_Current_Target - Targets the brake current of the motor
+*/
+uint8_t send_brake_current_command
+(float brake_ac_current);
+
+/**
+* Contents of this message:
+* VCU/Commands/Drive_Enable_Target - Drive allowed
+*/
+uint8_t send_drive_enable_command
+(uint8_t drive_enable);
 #endif
